cc29b40b97d00ad02b6c150b6af9ceafa392abec
Pawan <poojary.pawan16@gmail.com>
2020-04-11 01:30:28 +0530

    added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -35,6 +35,71 @@ switch (profile) {
         console.log("uekya abb");
     break;
     default: console.log("uje less banda");
-    break;
+
 }
 
+// 3.
+var johnscore = [89,120,1103];
+var mikescore = [116,94,123];
+
+    var johnavg = (johnscore.reduce(add))/3;
+//     console.log(johnscore.reduce(add));
+//     console.log(johnavg);
+    var mikeavg = (mikescore.reduce(add))/3;
+//     console.log(mikescore.reduce(add));
+//     console.log(mikeavg);
+    
+    if(johnavg < mikeavg){
+        console.log("cha gya mike bhia");
+        console.log(mikeavg);
+    }else if(johnavg == mikeavg){
+        console.log("wasn't expecting a draw");
+     }
+
+    else {
+        console.log("welldone john");
+        console.log(johnavg);
+    }
+
+
+    
+
+function add(a,b){
+//     console.log(a,b)
+    return a+b;
+}
+
+
+// 3.b
+var johnscore = [89,120,103];
+var mikescore = [116,94,123];
+var maryscore =  [97,134,105];
+    var johnavg = (johnscore.reduce(add))/3;
+//     console.log(johnscore.reduce(add));
+//     console.log(johnavg);
+    var mikeavg = (mikescore.reduce(add))/3;
+//     console.log(mikescore.reduce(add));
+//     console.log(mikeavg);
+    var maryavg = (maryscore.reduce(add))/3;
+
+    if(mikeavg>johnavg && mikeavg>maryavg){
+        console.log("cha gya mike bhia");
+        console.log(mikeavg);
+    }else if(johnavg > mikeavg && johnavg > maryavg){
+        console.log("john is the winner");
+     }
+    else if(maryavg>johnavg && maryavg>mikeavg){
+        console.log("Mary you win");
+    }
+    else {
+        console.log("bored");
+        console.log(johnavg);
+    }
+
+
+    
+
+function add(a,b){
+//     console.log(a,b)
+    return a+b;
+}
