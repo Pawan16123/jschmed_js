feb3078767b06784b3ce09fa47e41c2920870021
Pawan <poojary.pawan16@gmail.com>
2020-06-04 23:55:25 +0530

    trial

--- assets/scripts/promptquestionnaire.js
+++ assets/scripts/promptquestionnaire.js
@@ -7,3 +7,22 @@ function cal(){
 }
 cal();
 // jsut for two day rule
+
+
+function Questions(question,answers,option){
+    this.question =  question;
+    this.anwers = answers;
+    this.option = option;
+    
+}
+Questions.prototype.displayques(){
+    console.log(this.question);
+}
+
+var q1 = new Question("Is js the best programming language", ["hell yeah", "Meh"]), 0);
+
+var q2 = new Question("Who's course is this",["Jonas","colt","max"],0);
+
+var q3 = new Question("Is coding",["boring","hard","fun","tedious"],2);
+
+var n = Math.floor(Math.random()*questions.length);
